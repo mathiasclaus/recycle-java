e4f4efe0234cc0e6cc2d92cace03a9fc94fdcccc
MauraLemaire <maura.lemaire@conundra.eu>
2023-05-12 13:57:19 +0200

    first scenario: cost of construction weight

--- src/main/java/com/dddeurope/recycle/spring/MainController.java
+++ src/main/java/com/dddeurope/recycle/spring/MainController.java
@@ -2,6 +2,7 @@ package com.dddeurope.recycle.spring;
 
 import com.dddeurope.recycle.commands.CommandMessage;
 import com.dddeurope.recycle.events.EventMessage;
+import com.dddeurope.recycle.events.FractionWasDropped;
 import com.dddeurope.recycle.events.PriceWasCalculated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,13 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
 public class MainController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+    private static final double CONSTRUCTION_WASTE_COST = 0.15;
 
     @GetMapping("/validate")
     public String validate() {
@@ -29,21 +30,36 @@ public class MainController {
     public ResponseEntity<EventMessage> handle(@RequestBody RecycleRequest request) {
         LOGGER.info("Incoming Request: {}", request.asString());
 
-        var message = new EventMessage("todo", new PriceWasCalculated("123", 1, "EUR"));
+        var cost = processRequest(request);
+
+        var message = new EventMessage("todo", new PriceWasCalculated("123", cost, "EUR"));
 
         return ResponseEntity.ok(message);
+    }
+
+    private double processRequest(RecycleRequest request) {
+        var history = request.history();
+        List<FractionWasDropped> fractionWasDroppedEvents = history.stream()
+            .filter(event -> "FractionWasDropped".equals(event.getType()))
+            .map(event -> (FractionWasDropped) event.getPayload())
+            .toList();
+
+        var constructionWasteWeight = fractionWasDroppedEvents.stream()
+            .filter(event -> "Construction waste".equals(event.fractionType()))
+            .mapToDouble(FractionWasDropped::weight)
+            .sum();
+
+        return constructionWasteWeight * CONSTRUCTION_WASTE_COST;
     }
 
     public record RecycleRequest(List<EventMessage> history, CommandMessage command) {
 
         public String asString() {
             var historyAsString = history.stream()
-                    .map(EventMessage::toString)
-                    .collect(Collectors.joining("\n\t"));
+                .map(EventMessage::toString)
+                .collect(Collectors.joining("\n\t"));
 
             return String.format("%n%s %nWith History\n\t%s", command, historyAsString);
         }
-
     }
-
 }
